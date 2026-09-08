e28b0de13931b1c074868b74eb2867d3b118570f
Hassan Ahmed <your-email@example.comhafizhassanahmedqadri123@gmail.com>
2026-09-08 19:43:05 +0500

    baseURL changed into vercel

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://my-academy-umber.vercel.app/api",
 });
 
 // Automatic JWT Token Attachment
