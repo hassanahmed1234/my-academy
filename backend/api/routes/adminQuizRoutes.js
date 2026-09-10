b69c6dc6171a9ce0526e8da5fc071a3727df5ed3
Hassan Ahmed <your-email@example.comhafizhassanahmedqadri123@gmail.com>
2026-09-10 20:02:33 +0500

    undo

--- backend/api/routes/adminQuizRoutes.js
+++ backend/api/routes/adminQuizRoutes.js
@@ -14,7 +14,7 @@ const router = express.Router();
 
 router.use(protect, adminOnly);
 
-router.get("/quizzes", getAllQuizzes); // <--- Add GET route for API.get("/quizzes")
+router.get("/", getAllQuizzes); // <--- Add GET route for API.get("/quizzes")
 router.post("/", createQuiz);
 router.put("/:quizId", updateQuiz);
 router.delete("/:quizId", deleteQuiz);
