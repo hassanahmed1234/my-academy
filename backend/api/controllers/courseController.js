dea00350a35908b7b9ec409cc0aa9663c23a49ad
Hassan Ahmed <your-email@example.comhafizhassanahmedqadri123@gmail.com>
2026-09-16 12:42:48 +0500

    undo

--- backend/api/controllers/courseController.js
+++ backend/api/controllers/courseController.js
@@ -15,7 +15,7 @@ export const getCourseById = async (req, res) => {
     const course = await Course.findById(req.params.id).lean();
 
     if (course) {
-      return res.status(200).json({data:course});
+      return res.status(200).json(course);
     } else {
       return res.status(404).json({ message: "Course not found" });
     }
