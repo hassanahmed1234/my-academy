a4ba5d074b27675e93f43ea0ddf80b844a5b735e
Hassan Ahmed <your-email@example.comhafizhassanahmedqadri123@gmail.com>
2026-09-19 13:35:19 +0500

    addglobalaward remove

--- backend/api/routes/myProgressRoutes.js
+++ backend/api/routes/myProgressRoutes.js
@@ -2,8 +2,7 @@ import express from "express";
 import { 
   getCourseProgress, 
   getAllUserProgress, 
-  markLessonComplete,
-  addGlobalXp
+  markLessonComplete
 } from "../controllers/myProgressController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
