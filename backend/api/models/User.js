3c90e3dfbc7945997b7fb443be75b62e02ff15d6
Hassan Ahmed <your-email@example.comhafizhassanahmedqadri123@gmail.com>
2026-09-19 16:01:03 +0500

    next() remove

--- backend/api/models/User.js
+++ backend/api/models/User.js
@@ -68,11 +68,11 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+// FIXED PRE-SAVE HOOK (Removed 'next' parameter for async/await)
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
